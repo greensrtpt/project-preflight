28950dfdcce17f31caaedcaa296f936278e36410
kanthidaja <kanthida_khun@cmu..ac.th>
2026-07-29 00:16:29 +0700

    add essential info for seed

--- pf-frontend/src/components/Background.tsx
+++ pf-frontend/src/components/Background.tsx
@@ -9,12 +9,12 @@ export const Background: React.FC<BackgroundLayoutProps> = ({ children }) => {
     <div className="min-h-screen w-full relative overflow-hidden bg-[#fbf9f4] transition-colors z-0">
       {/* 🌟 ตัวแปร Mesh Gradient สำหรับ Light Mode */}
       <div 
-        className="absolute inset-0 pointer-events-none opacity-80 dark:opacity-20 transition-opacity"
+        className="absolute inset-0 pointer-events-none opacity-100 transition-opacity"
         style={{
           backgroundImage: `
-            radial-gradient(at 0% 100%, #F2BEED 10px, transparent 50%),
-            radial-gradient(at 50% 50%, #F3F5E7 10px, transparent 90%),
-            radial-gradient(at 30% 30%, #F3F5E7 10px, transparent 90%),
+            radial-gradient(at 0% 100%, #F2BEED 0px, transparent 50%),
+            radial-gradient(at 50% 50%, #F3F5E7 0px, transparent 100%),
+            radial-gradient(at 30% 30%, #F3F5E7 0px, transparent 100%),
             radial-gradient(at 100% 10%, #C1E5EF 10px, transparent 100%)
           `
         }}
